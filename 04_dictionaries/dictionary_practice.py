f5e69e0f4f09defab58abcbeb5856d61d95ca5f8
Bern <bernardorquez@gmail.com>
2026-09-02 13:10:32 +0800

    Update dictionary_practice.py

--- 04_dictionaries/dictionary_practice.py
+++ 04_dictionaries/dictionary_practice.py
@@ -1,1 +1,31 @@
+# Dictionary Practice
+# Topic: Storing and accessing information
 
+
+# Creating a dictionary
+
+student = {
+    "name": "Bern",
+    "age": 20,
+    "course": "Computer Science"
+}
+
+
+# Accessing values
+
+print(student["name"])
+print(student["course"])
+
+
+# Adding new information
+
+student["year"] = 1
+
+print(student)
+
+
+# Changing information
+
+student["age"] = 21
+
+print(student)
