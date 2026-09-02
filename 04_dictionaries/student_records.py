da6cff5c6f8a0b92fcb337600a96d62cb38b9670
Bern <bernardorquez@gmail.com>
2026-09-02 13:12:44 +0800

    Create student_records.py

---
+++ 04_dictionaries/student_records.py
@@ -0,0 +1,21 @@
+# Lists of Dictionaries Practice
+
+students = [
+    {
+        "name": "Alice",
+        "grade": 90
+    },
+    {
+        "name": "Bob",
+        "grade": 75
+    },
+    {
+        "name": "Charlie",
+        "grade": 60
+    }
+]
+
+# Accessing information
+
+print(students[0]["name"])
+print(students[1]["grade"])
