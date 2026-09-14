6a3099254b4782035dac6af803e55d9abd5269a0
Bern <bernardorquez@gmail.com>
2026-09-14 22:16:42 +0800

    Update abstraction_shapes.py

--- 05_oop/abstraction_shapes.py
+++ 05_oop/abstraction_shapes.py
@@ -1,1 +1,65 @@
+from abc import ABC, abstractmethod
+import math
+
+class Shape(ABC):
+
+    @abstractmethod
+    def area(self):
+        pass
+
+    @abstractmethod
+    def perimeter (self):
+        pass
+
+    def describe(self):
+        print(
+            f'{self.__class__.__name__} -> '
+            f'Area: {self.area():>5.2f} | '
+            f'Perimeter: {self.perimeter():>5.2f}'
+        )
+
+class Rectangle(Shape):
+
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return self.length * self.width
+
+    def perimeter(self):
+        return 2 * (self.length + self.width)
+
+
+class Circle(Shape):
+
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return math.pi * (self.radius ** 2)
+
+    def perimeter(self):
+        return 2 * math.pi * self.radius
+
+
+class Triangle(Shape):
+
+    def __init__(self, a, b, c):
+        self.a = a
+        self.b = b
+        self.c = c
+
+    def area(self):
+        s = (self.a + self.b + self.c) / 2
+        return math.sqrt(s * (s - self.a) * (s - self.b) * (s - self.c))
+
+    def perimeter(self):
+        return self.a + self.b + self.c
+
+if __name__ == "__main__":
+    shapes = [Rectangle(4, 6), Circle(5), Triangle(3, 4, 5)]
+
+    for s in shapes:
+        s.describe()
 
